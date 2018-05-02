24deafd30ff463d4e66fddd203cbeed129b2168a
JUHO\juho <wngh9512@naver.com>
2018-05-02 17:19:23 +0900

    가상 범죄자 만들기 위해서 성별과 나이 추출 및 개인적으로 나이에 관한 데이터프레임 만드는 코드 짜오기

--- crime.R
+++ crime.R
@@ -1,13 +1,3 @@
 setwd("D:/crime")
 
-crimeTime = read.csv("crimeTime.csv")
-class(crimeTime)
-
-crimeTimeData = data.frame(crimeTime[4,])
-crimeTimeData = rbind(crimeTimeData, crimeTime[6,])
-crimeTimeData = rbind(crimeTimeData, crimeTime[7,])
-crimeTimeData = rbind(crimeTimeData, crimeTime[12,])
-crimeTimeData = rbind(crimeTimeData, crimeTime[13,])
-crimeTimeData = rbind(crimeTimeData, crimeTime[16,])
-
-write.csv(crimeTimeData,"crimeTimeData.csv")
+crimeSexAndAge = read.csv("sexAndage.csv")
