f9c202284182aa3606b5541e4551a5f9822a598f
Hello World\Hello world <otl9882@naver.com>
2018-05-28 17:24:15 +0900

    spot 제외하고 현재까지 완성된 데이터로 barplot 완성!!

--- crimeplot.R
+++ crimeplot.R
@@ -29,30 +29,31 @@ time = table(Total_df$Time)
 type = table(Total_df$Type)
 
 #Previous
-#previous = table(Total_df$Previous)
+previous = table(Total_df$Previous)
 
 #Crime spot
 #spot = table(Total_df$Spot)
 
 
-barplot(age,xlab = "Age",ylab = "number of persons",main = "Age criminal",col="black")
+barplot(age,xlab = "Age",ylab = "number of persons",main = "Age criminal",col="black",names=c("10~19","20~29","30~39","40~49","50~59","60~69","70~79"))
 
-barplot(gender,xlab = "0 Woman | 1 Man",ylab = "number of persons",main = "Gender criminal",col="black")
+barplot(gender,xlab = "Gender",ylab = "number of persons",main = "Gender criminal",col="black",names=c("Woman","Man"))
 
-barplot(money,xlab = "1 Low | 2 Mid | High",ylab = "number of persons",main = "Income criminal",col="black")
+barplot(money,xlab = "Money",ylab = "number of persons",main = "Income criminal",col="black",names=c("Low","Mid","High"))
 
-barplot(education,xlab = "",ylab = "number of persons",main = "Education criminal",col="black")
+barplot(education,xlab = "Education",ylab = "number of persons",main = "Education criminal",col="black",names=c("Elementary","Middle","High","Univ2","Univ4","Graduate"))
 
-barplot(parents,xlab = "1 Marriage",ylab = "number of persons",main = "Gender criminal",col="black")
-1 부모O / 2 아빠만 /  3 엄마만 / 4 없쯤
-barplot(marriage,xlab = "0 Woman | 1 Man",ylab = "number of persons",main = "Gender criminal",col="black")
+barplot(parents,xlab = "Parents",ylab = "number of persons",main = "Parents criminal",col="black",names=c("Parents","Father" ,"Mother","Orphan"))
 
-barplot(day,xlab = "0 Woman | 1 Man",ylab = "number of persons",main = "Gender criminal",col="black")
+barplot(marriage,xlab = "Marriage",ylab = "number of persons",main = "Marriage criminal",col="black",names=c("Marriage", "LiveTogether","divorce","bereavement"))
 
-barplot(time,xlab = "0 Woman | 1 Man",ylab = "number of persons",main = "Gender criminal",col="black")
+barplot(day,xlab = " Day ",ylab = "number of persons",main = "Crime Day criminal",col="black",names=c("Mon","TUE","WED","THU","FRI","SAT","SUN"))
 
-barplot(type,xlab = "0 Woman | 1 Man",ylab = "number of persons",main = "Gender criminal",col="black")
+barplot(time,xlab = " Time ",ylab = "number of persons",main = "Crime Time criminal",col="black",names=c("00~03","~06","~09","~12","~15","~18","~21","~24"))
 
-barplot(previous,xlab = "0 Woman | 1 Man",ylab = "number of persons",main = "Gender criminal",col="black")
+barplot(type,xlab = "Type",ylab = "number of persons",main = "Crime Type criminal",col="black",names=c("Muder","Robber","Rape","Arson","Theft","Assault"))
 
-barplot(spot,xlab = "0 Woman | 1 Man",ylab = "number of persons",main = "Gender criminal",col="black")
+barplot(previous,xlab = "Previous",ylab = "number of persons",main = "Previous criminal",col="black",names=c("No","Yes"))
+pie(previous)
+
+#barplot(spot,xlab = "0 Woman | 1 Man",ylab = "number of persons",main = "Crime Spot",col="black")
