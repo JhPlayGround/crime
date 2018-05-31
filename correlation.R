a511938cf670579d014dc77924dfa47070466287
JUHO\juho <wngh9512@naver.com>
2018-05-31 21:48:25 +0900

    범례 추가 코드 barplot 함수 아래에 작성했어 주환.

--- correlation.R
+++ correlation.R
@@ -14,3 +14,27 @@ chart.Correlation(criminal,histogram = TRUE, pch=19)
 criminal.cor = cor(criminal)
 corrplot(criminal.cor,method="number")
 corrplot(criminal.cor,method="square")
+
+
+Total_df = read.csv("criminal.csv")
+Total_df = Total_df[,c(1:2)]
+
+a=table(Total_df)
+table(Total_df)
+prop.table(Total_df)
+
+barplot(t(a), beside=TRUE)
+legend("topright", c("여자", "남자"), cex=0.8, fill =c("black","gray"))
+
+
+chisq.test(a)
+chisq.test_output_2 = chisq.test(a)
+chisq.test_output_2$observed
+chisq.test_output_2$expected
+chisq.test_output_2$residuals
+chisq.test_output_2$statistic
+chisq.test_output_2$parameter
+chisq.test_output_2$p.value
+
+?chisq.test
+
