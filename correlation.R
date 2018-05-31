4f75ecc31ec9257b5af40042b858d4c39dc2d9fa
JUHO\juho <wngh9512@naver.com>
2018-05-31 23:50:58 +0900

    나이 + 소득관계 plot 생성

--- correlation.R
+++ correlation.R
@@ -11,3 +11,10 @@ criminalAG = table(criminalAG)
 criminalAG
 barplot(t(criminalAG), beside=TRUE)
 legend("topright", c("Women", "Men"), cex=1, fill =c("black","gray"))
+
+
+criminalAM = criminal[,c(1,3)]
+criminalAM = table(criminalAM)
+criminalAM
+barplot(t(criminalAM), beside=TRUE, col=c("black","gray","red"))
+legend("topright", c("Low", "Middle","High"), cex=1, fill =c("black","gray","red"))
